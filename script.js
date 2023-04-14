c6f3ce2a33f056c6694bbaef653d25d42f5745a9
Prince Chaurasia <princechaurasiaofficial24@gmail.com>
2023-04-14 16:48:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const inputs = document.querySelectorAll("input"),
+  buttons = document.querySelector("button");
+
+console.log(inputs);
+
+// focus on first input box on reload
+window.addEventListener("load", () => inputs[0].focus());
+
+inputs.forEach((input, index1) => {
+  input.addEventListener("keyup", (e) => {
+    const currentInput = input,
+      nextInput = input.nextElementSibling,
+      prevInput = input.previousElementSibling;
+
+    // if the current input has more than 1 character erase the value
+
+    if (currentInput.value.length > 1) {
+      currentInput.value = "";
+      return;
+    }
+
+    // If the currentInput is not empty then enable the nextInput
+
+    if (
+      nextInput &&
+      nextInput.hasAttribute("disabled") &&
+      currentInput.value !== ""
+    ) {
+      nextInput.removeAttribute("disabled");
+      nextInput.focus();
+    }
+
+    // when backspace is pressed
+
+    if (e.key === "Backspace") {
+      inputs.forEach((input, index2) => {
+        if (index1 <= index2 && prevInput) {
+          input.setAttribute("disabled", true);
+          currentInput.value = "";
+          prevInput.focus();
+        }
+      });
+    }
+
+    // making verify button active and inactive when the last input is done
+
+    if (!inputs[3].disabled && inputs[3].value !== "") {
+      buttons.classList.add("active");
+      return;
+    }
+    buttons.classList.remove("active");
+  });
+});
